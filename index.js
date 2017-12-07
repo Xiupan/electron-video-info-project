f50a5c744970805aa8f15c26e11258040baa2d02
Alan Hong <xiupan@gmail.com>
2017-12-06 22:56:50 -0600

    Added some comments to clarify what's going on between the Electron App and the HTML page.

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ app.on('ready', () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`)
 })
 
+// this receives information from the html
 ipcMain.on('videoSubmitted', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
     console.log(`Video length is: ${metadata.format.duration} seconds.`);
+    // this sends information to the html
     mainWindow.webContents.send('videoGotMetadata', metadata.format.duration)
   })
 })
