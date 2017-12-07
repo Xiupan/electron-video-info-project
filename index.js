93a8aaaefd09156b4ed2a4cc3838db305f403a49
Alan Hong <xiupan@gmail.com>
2017-12-06 22:37:07 -0600

    HTML sends info to the Electron app via ipcRenderer.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg')
 
 app.on('ready', () => {
   const mainWindow = new BrowserWindow({})
   mainWindow.loadURL(`file://${__dirname}/index.html`)
+})
+
+ipcMain.on('videoSubmitted', () => {
+  console.log(ffmpeg.ffprobe);
 })
